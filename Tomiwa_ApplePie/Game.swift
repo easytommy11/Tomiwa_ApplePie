59b3821afe255c9e01dde839927c749d8a7002a7
Tomiwa Babalola <tbabalola004@gmail.com>
2021-11-10 09:33:33 -0500

    vhf

---
+++ Tomiwa_ApplePie/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  Tomiwa_ApplePie
+//
+//  Created by Tomiwa Babalola on 11/10/21.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Tomiwa_ApplePie/ViewController.swift
+++ Tomiwa_ApplePie/ViewController.swift
@@ -7,15 +7,49 @@
 
 import UIKit
 
+var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+
+let incorrectMovesAllowed = 7
+
+var totalWins = 0
+var totalLosses = 0
 class ViewController: UIViewController {
 
     
     @IBOutlet var treeImageView: UIImageView!
     
+    @IBOutlet var correctWordLabel: UILabel!
+    
+    @IBOutlet var scoreLabel: UILabel!
+    
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view.
     }
+    
+    var currentGame: Game!
+    func newRound(){
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining:
+        incorrectMovesAllowed)
+        updateUI()
+
+    }
+    func updateUI() {
+    scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+    treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+
+    }
+
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false 
+    }
+    
 
 
 }
